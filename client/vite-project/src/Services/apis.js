063661a801501fb89bb5ed24dc6543d4e3fcbb5c
Azan-imtiaz <azanimtiaz43@gmail.com>
2024-09-18 21:18:41 +0500

    changes

--- client/vite-project/src/Services/apis.js
+++ client/vite-project/src/Services/apis.js
@@ -1,6 +1,6 @@
 // api.js
 // const API_BASE_URL = 'https://comforttrips.co.uk:3004'; // Replace with your actual API base URL
- const API_BASE_URL = 'http://localhost:8000'; // Replace with your actual API base URL
+ const API_BASE_URL = 'http://localhost:9001'; // Replace with your actual API base URL
 
 
 export const signIn = async (email, password) => {
