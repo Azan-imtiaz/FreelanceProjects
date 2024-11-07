0074530ebbf62b70be89d7db58b0908680c038af
Azan-imtiaz <azanimtiaz43@gmail.com>
2024-11-07 21:54:10 +0500

    completed

--- client/vite-project/src/Services/helper.js
+++ client/vite-project/src/Services/helper.js
@@ -1,2 +1,2 @@
-export const Base_Url="http://localhost:9002";
-// export const Base_Url="https://comforttrips.co.uk:3004"
+// export const Base_Url="http://localhost:9002";
+export const Base_Url="https://comforttrips.co.uk:3004"
